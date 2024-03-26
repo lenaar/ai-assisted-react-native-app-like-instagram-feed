6f594499156a6b4afaee93d3bc0edbdc283ea2a1
Elena <elena@dtr1tech.com>
2024-03-26 12:13:36 +0100

    feat: list and style instagram feed

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, FlatList } from 'react-native';
+import data from './instagram-feed/data';
 
 export default function App() {
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Image style={styles.image} source={item.image} />
+      <Text style={styles.name}>{item.name}</Text>
+      <Text style={styles.detailText}>Likes: {item.likes}</Text>
+      <Text style={styles.detailText}>Comments: {item.commentCount}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.header}>Instagram Feed Application</Text>
+      <FlatList
+        data={data.articles}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        style={styles.list}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffc0cb',
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+    textAlign: 'center',
+  },
+  list: {
+    width: '100%', // Makes FlatList take the full width
+  },
+  itemContainer: {
+    backgroundColor: 'white',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 10,
+    borderRadius: 10,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    width: '100%', // Adjust as needed
+    height: 200, // Adjust as needed
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  detailText: {
+    fontSize: 14,
   },
 });
+
